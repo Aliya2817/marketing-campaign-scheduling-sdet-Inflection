ffa399bdb98c4592b75cf6da1ef81aa7d03142de
Aliya Kumari <kumarialiya36@gmail.com>
2024-06-05 23:43:10 +0530

    Aliya | [feat] test fetch campaign added

--- test_campaign/test_create_campaign.py
+++ test_campaign/test_create_campaign.py
@@ -152,11 +152,8 @@ def test_successful_create_campaign(successful_campaign_payload):
 
 def test_update_campaign_with_invalid_campaign_id(invalid_campaign_id, valid_campaign_name):
     url = f"{settings.CAMPAIGN_SERVICE_BASE_URL}/{invalid_campaign_id}/name"
-
     payload = json.dumps({"campaignName": valid_campaign_name})
-
     headers = {'accept': '*/*', 'Content-Type': 'application/json'}
-
     response = requests.request("PATCH", url, headers=headers, data=payload)
 
     # Validate the response status code
@@ -184,9 +181,7 @@ def test_successful_update_campaign(successful_campaign_payload, valid_campaign_
     campaign_id = response.json()['data']['id']
 
     url = f"{settings.CAMPAIGN_SERVICE_BASE_URL}/{campaign_id}/name"
-
     payload = json.dumps({"campaignName": valid_campaign_name})
-
     headers = {'accept': '*/*', 'Content-Type': 'application/json'}
 
     response = requests.request("PATCH", url, headers=headers, data=payload)
@@ -208,3 +203,65 @@ def test_successful_update_campaign(successful_campaign_payload, valid_campaign_
 
     # Validate meta content
     assert meta['status'] == 'SUCCESS'
+
+
+def test_successful_fetch_campaign(successful_campaign_payload):
+    # First creating a campaign to fetch the same
+    response = create_campaign(successful_campaign_payload)
+    campaign_id = response.json()['data']['id']
+
+    url = f"{settings.CAMPAIGN_SERVICE_BASE_URL}/{campaign_id}"
+    headers = {'accept': '*/*', 'Content-Type': 'application/json'}
+    response = requests.request("GET", url, headers=headers)
+
+    # Validate the response status code
+    assert response.status_code == 200
+
+    response_data = response.json()
+
+    # Validate the response structure
+    assert 'data' in response_data
+    assert 'meta' in response_data
+
+    data = response_data['data']
+    meta = response_data['meta']
+
+    # Validate the data content
+    assert data['campaignName'] == successful_campaign_payload['campaignName']
+    assert data['emailTemplateId'] == successful_campaign_payload['emailTemplateId']
+    assert data['recipientListId'] == successful_campaign_payload['recipientListId']
+    assert data['scheduledTime'] == successful_campaign_payload['scheduledTime']
+
+    # Validate meta content
+    assert meta['status'] == 'SUCCESS'
+
+    # Validate the timestamp format in meta
+    try:
+        datetime.fromisoformat(meta['timestamp'].replace('Z', '+00:00'))
+    except ValueError:
+        assert False, "Timestamp format is invalid"
+
+
+def test_fetch_campaign_with_invalid_campaign_id(invalid_campaign_id, valid_campaign_name):
+    url = f"{settings.CAMPAIGN_SERVICE_BASE_URL}/{invalid_campaign_id}"
+    headers = {'accept': '*/*', 'Content-Type': 'application/json'}
+    response = requests.request("GET", url, headers=headers)
+
+    # Validate the response status code
+    assert response.status_code == 404
+
+    response_data = response.json()
+
+    # Validate the response structure
+    assert 'errors' in response_data
+    assert 'meta' in response_data
+
+    errors = response_data['errors']
+    meta = response_data['meta']
+
+    # Validate the error code
+    assert len(errors) > 0
+    assert errors[0]['errorCode'] == 'CAM-E-001'
+
+    # Validate meta content
+    assert meta['status'] == 'FAILURE'
